e929a016dd69ea734f0510681ab9982e0cf11895
kevinnsrt <siraitkevin25@gmail.com>
2024-12-03 14:52:24 +0700

    first commit

---
+++ src/Form/SessionData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Form;
+
+/**
+ *
+ * @author nitro
+ */
+public class SessionData {
+    public static int loggedInUserId = 0; // Default: tidak ada user login
+    public static String loggedInUserName = null; // Default: tidak ada nama user
+}
+
